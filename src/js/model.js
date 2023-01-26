65675ed92f1958de1618bdbe2a28dd5ee3f20cf7
Adarsh Desai <desaiadarsh22@gmail.com>
2023-01-26 11:37:31 +0530

    Added products and pricing to model

---
+++ src/js/model.js
@@ -0,0 +1,26 @@
+plans = [
+  { id: 1, name: "Arcade", monthlyPrice: 9, yearlyPrice: 90 },
+  { id: 2, name: "Advanced", monthlyPrice: 12, yearlyPrice: 120 },
+  { id: 3, name: "Pro", monthlyPrice: 15, yearlyPrice: 150 },
+];
+
+addons = [
+  {
+    id: 1,
+    name: "Online Services",
+    monthlyPrice: 1,
+    yearlyPrice: 10,
+  },
+  {
+    id: 1,
+    name: "Online Services",
+    monthlyPrice: 2,
+    yearlyPrice: 20,
+  },
+  {
+    id: 1,
+    name: "Online Services",
+    monthlyPrice: 2,
+    yearlyPrice: 20,
+  },
+];
